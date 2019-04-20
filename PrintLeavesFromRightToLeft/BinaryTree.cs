85c045572355d95d88dc73bee36f36b46d3ee1f2
Giruba Beulah SE <gibeulah@microsoft.com>
2019-04-20 23:42:39 +0530

    Add project files.

---
+++ PrintLeavesFromRightToLeft/BinaryTree.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrintLeavesFromRightToLeft
+{
+    class BinaryTree
+    {
+        BinaryTreeNode root;
+
+        public BinaryTree(BinaryTreeNode binaryTreeNode) {
+            root = binaryTreeNode;
+        }
+
+        public void SetBinaryTreeNodeRoot(BinaryTreeNode binaryTreeNode) {
+            root = binaryTreeNode;
+        }
+
+        public BinaryTreeNode GetBinaryTreeRoot() {
+            return root;
+        }
+
+        public BinaryTreeNode Insert(BinaryTreeNode node, int data) {
+            if (node == null) {
+                node = new BinaryTreeNode(data);
+                return node;
+            }
+
+            if (node.GetBinaryTreeNodeData() < data)
+            {
+                node.SetBinaryTreeNodeRight(Insert(node.GetBinaryTreeNodeRight(),
+                    data));
+            }
+            else {
+                node.SetBinaryTreeNodeLeft(Insert(node.GetBinaryTreeNodeLeft(),
+                    data));
+            }
+
+            return node;
+        }
+
+        public void PrintLeavesFromRightToLeft(BinaryTreeNode binaryTreeNode) {
+            if (binaryTreeNode == null) {
+                return;
+            }
+            if (binaryTreeNode.GetBinaryTreeNodeLeft() == null &&
+                binaryTreeNode.GetBinaryTreeNodeRight() == null) {
+                Console.Write(binaryTreeNode.GetBinaryTreeNodeData()+ " ");
+            }
+
+            PrintLeavesFromRightToLeft(binaryTreeNode.GetBinaryTreeNodeRight());
+            PrintLeavesFromRightToLeft(binaryTreeNode.GetBinaryTreeNodeLeft());
+
+        }
+
+        public void InorderTraversal(BinaryTreeNode binaryTreeNode) {
+            if (binaryTreeNode == null) {
+                return;
+            }
+            InorderTraversal(binaryTreeNode.GetBinaryTreeNodeLeft());
+            Console.Write(binaryTreeNode.GetBinaryTreeNodeData()+" ");
+            InorderTraversal(binaryTreeNode.GetBinaryTreeNodeRight());
+
+        }
+    }
+}
---
+++ PrintLeavesFromRightToLeft/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace PrintLeavesFromRightToLeft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Print Leaf nodes from right to left!");
+            Console.WriteLine("------------------------------------");
+
+            BinaryTree binaryTree = GetBinaryTreeFromInput();
+            Console.WriteLine();
+            Console.WriteLine("---Inorder traversal---");
+            binaryTree.InorderTraversal(binaryTree.GetBinaryTreeRoot());
+            Console.WriteLine();
+            Console.WriteLine("---Printing Leaves from Right to Left----");
+            binaryTree.PrintLeavesFromRightToLeft(binaryTree.GetBinaryTreeRoot());
+
+
+            Console.ReadLine();
+        }
+
+        private static BinaryTree GetBinaryTreeFromInput() {
+            BinaryTree binaryTree = null;
+
+            Console.WriteLine("Enter the number of elements in the" +
+                " binary tree");
+            try
+            {
+                int numberElements = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the elements separated by" +
+                    " comma, semi-colon or space");
+                String[] elements = Console.ReadLine().Split(' ', ',', ';');
+                BinaryTreeNode binaryTreeNode = null;
+                binaryTree = new BinaryTree(binaryTreeNode);
+                for (int index = 0; index < numberElements; index++) {
+                    binaryTreeNode = binaryTree.Insert(binaryTreeNode,
+                        int.Parse(elements[index]));
+                }
+                binaryTree.SetBinaryTreeNodeRoot(binaryTreeNode);
+            }
+            catch (Exception exception) {
+                Console.WriteLine("Thrown exception is" +
+                    " "+exception.Message);
+            }
+
+
+            return binaryTree;
+
+        }
+    }
+}
